a686d75c9969cdfa5747d7dac6bd6eeae65c6cf3
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2020-04-16 12:04:33 +0200

    update build script for windows

--- build-win32-bins.bat
+++ build-win32-bins.bat
@@ -1,5 +1,5 @@
 @echo off
 cd "%~dp0"
-"C:\WinPython\WPy32-3810\python-3.8.1\Scripts\pyinstaller.exe" --hidden-import Crypto.PublicKey --hidden-import Crypto.Hash --hidden-import Crytpro.Signature --hidden-import PySide2.QtXml --hidden-import PySide2.QtQuick --noconfirm --name Pwtester-x86
-"C:\WinPython\WPy64-3810\python-3.8.1.amd64\Scripts\pyinstaller.exe" --hidden-import Crypto.PublicKey --hidden-import Crypto.Hash --hidden-import Crytpro.Signature --hidden-import PySide2.QtXml --hidden-import PySide2.QtQuick --noconfirm --name Pwtester-x86_64
+"C:\WinPython\WPy32-3810\python-3.8.1\Scripts\pyinstaller.exe" --hidden-import Crypto.PublicKey --hidden-import Crypto.Hash --hidden-import Crytpro.Signature --hidden-import PySide2.QtXml --hidden-import PySide2.QtQuick --noconfirm --name Pwtester-x86 --window main.py
+"C:\WinPython\WPy64-3810\python-3.8.1.amd64\Scripts\pyinstaller.exe" --hidden-import Crypto.PublicKey --hidden-import Crypto.Hash --hidden-import Crytpro.Signature --hidden-import PySide2.QtXml --hidden-import PySide2.QtQuick --noconfirm --name Pwtester-x86_64 --window main.py
 pause
