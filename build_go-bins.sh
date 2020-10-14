cc1a0b41bfa478def38de3521f6c2904c383f228
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2020-10-14 21:10:54 +0200

    build script for linux and windows binaries

--- build_go-bins.sh
+++ build_go-bins.sh
@@ -6,8 +6,6 @@ mv "$SCRIPTDIR/deploy/linux" "$SCRIPTDIR/deploy/pwtester_linux-x86_64"
 # Cleanup when we are done
 rm -r "$SCRIPTDIR/linux"
 
-echo Press enter to build the Windows binaries (requires that you have pulled the docker images from therecipe/qt)
-echo Press CTRL+C to exit
 $HOME/go/bin/qtdeploy -docker -ldflags -H=windowsgui build windows_64_shared
 mv "$SCRIPTDIR/deploy/windows" "$SCRIPTDIR/deploy/pwtester_windows-x86_64"
 $HOME/go/bin/qtdeploy -docker -ldflags -H=windowsgui build windows_32_shared
