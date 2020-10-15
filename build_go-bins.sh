9dd83f4cecd42d8d877020706b7d225ba2ae4347
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2020-10-15 13:13:05 +0200

    prepare for including icons

--- build_go-bins.sh
+++ build_go-bins.sh
@@ -3,13 +3,14 @@ SCRIPTDIR=`dirname $0`
 rm -r "$SCRIPTDIR/deploy"
 $HOME/go/bin/qtdeploy build linux
 mv "$SCRIPTDIR/deploy/linux" "$SCRIPTDIR/deploy/pwtester_linux-x86_64"
-# Cleanup when we are done
-rm -r "$SCRIPTDIR/linux"
+
+# Cleanup empty dirs when done (if we embed files into the binary, this wont be empty)
+rmdir "$SCRIPTDIR/linux"
 
 $HOME/go/bin/qtdeploy -docker -ldflags -H=windowsgui build windows_64_shared
 mv "$SCRIPTDIR/deploy/windows" "$SCRIPTDIR/deploy/pwtester_windows-x86_64"
 $HOME/go/bin/qtdeploy -docker -ldflags -H=windowsgui build windows_32_shared
 mv "$SCRIPTDIR/deploy/windows" "$SCRIPTDIR/deploy/pwtester_windows-x86"
 
-# Cleanup when we are done
-rm -r "$SCRIPTDIR/windows"
+# Cleanup empty dirs when done (if we embed files into the binary, this wont be empty)
+rmdir "$SCRIPTDIR/windows"
--- build_go-macOS-bins.command
+++ build_go-macOS-bins.command
@@ -4,5 +4,5 @@ rm -r "$SCRIPTDIR/deploy"
 $HOME/go/bin/qtdeploy build desktop
 mv "$SCRIPTDIR/deploy/darwin" "$SCRIPTDIR/deploy/pwtester_MacOS-x86_64"
 
-# Cleanup when we are done
-rm -r "$SCRIPTDIR/darwin"
+# Cleanup empty dirs when done (if we embed files into the binary, this wont be empty)
+rmdir "$SCRIPTDIR/darwin"
