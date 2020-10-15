f9bf87563031593b558a934a0808a7adef9d6b87
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2020-10-15 15:35:42 +0200

    assets for linux

---
+++ linux/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SCRIPTDIR=`dirname $0`
+printf "This script will install the contents of $SCRIPTDIR/ into /opt/pwtester/
+and copy the /opt/pwtester/assets/pwtester.desktop file to /usr/share/applications/.
+To uninstall the application just run /opt/pwtester/uninstall.sh.
+
+To continue press ENTER, to quit press CTRL+C\n\n"
+
+printf "Making /opt/pwtester/\n"
+sudo mkdir -p /opt/pwtester
+
+printf "\nCopying files to /opt/pwtester/\n"
+sudo cp -rv "$SCRIPTDIR" /opt/pwtester/
+
+printf "\nCopying pwtester.desktop to /usr/share/applications"
+cp -v /opt/pwtester/assets/pwtester.desktop /usr/share/applications/
---
+++ linux/uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SCRIPTDIR=`dirname $0`
+printf "This script will uninstall the contents of /opt/pwtester
+and remove the /usr/share/applications/pwtester.desktop file.
+
+To continue press ENTER, to quit press CTRL+C\n\n"
+
+printf "Removing the menu entry\n"
+sudo rm -v /usr/share/applications/pwtester.desktop
+
+printf "\nRemoving the application from /opt/pwtester\n"
+sudo rm -rfv /opt/pwtester
