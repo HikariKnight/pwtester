4490bc826d5d8db907f8901a35f9a1eca3ab518f
HikariKnight <2557889+HikariKnight@users.noreply.github.com>
2020-10-15 15:07:58 +0200

    show some info on the build progress

--- build_go-bins.sh
+++ build_go-bins.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 SCRIPTDIR=`dirname $0`
+echo Removing old builds
 rm -r "$SCRIPTDIR/deploy"
+
+echo Building linux binaries
 $HOME/go/bin/qtdeploy build linux
 mv "$SCRIPTDIR/deploy/linux" "$SCRIPTDIR/deploy/pwtester_linux-x86_64"
 
-# Cleanup empty dirs when done (if we embed files into the binary, this wont be empty)
-rmdir "$SCRIPTDIR/linux"
-
+echo Prepping icons for Windows x86_64
 x86_64-w64-mingw32-windres assets/icon.rc assets/icon_windows.syso 
+
+echo Building Windows x86_64 binaries
 $HOME/go/bin/qtdeploy -docker -ldflags -H=windowsgui build windows_64_shared
 mv "$SCRIPTDIR/deploy/windows" "$SCRIPTDIR/deploy/pwtester_windows-x86_64"
 
+echo Prepping icons for Windows x86
 i686-w64-mingw32-windres assets/icon.rc assets/icon_windows.syso
+
+echo Building Windows x86 binaries
 $HOME/go/bin/qtdeploy -docker -ldflags -H=windowsgui build windows_32_shared
 mv "$SCRIPTDIR/deploy/windows" "$SCRIPTDIR/deploy/pwtester_windows-x86"
 
--- build_go-macOS-bins.command
+++ build_go-macOS-bins.command
@@ -1,8 +1,9 @@
 #!/bin/bash
 SCRIPTDIR=`dirname $0`
+
+echo Removing old builds
 rm -r "$SCRIPTDIR/deploy"
+
+echo Building MacOS/darwin binaries
 $HOME/go/bin/qtdeploy build desktop
 mv "$SCRIPTDIR/deploy/darwin" "$SCRIPTDIR/deploy/pwtester_MacOS-x86_64"
-
-# Cleanup empty dirs when done (if we embed files into the binary, this wont be empty)
-rmdir "$SCRIPTDIR/darwin"
